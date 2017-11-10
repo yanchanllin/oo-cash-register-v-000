2287067b9ed2daa607ca6419310ddd8f276c721d
YanC Lin <yonalin1@gmail.com>
2017-11-10 16:03:03 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,46 +1,31 @@
 class CashRegister
-  attr_accessor :discount, :total, :last_transaction
 
+  attr_accessor :items, :discount, :total, :last_transaction
 
-  def initialize(discount = 0)
+  def initialize(discount=0)
     @total = 0
     @discount = discount
-    @cart = []
+    @items = []
   end
 
-  def add_item(item, price, quantity = 1)
-    item_info = {}
-    item_info[:name] = item
-    item_info[:price] = price
-    item_info[:quantity] = quantity
-
-    @cart << item_info
-
-    @total += price * quantity
-
+  def add_item(title, amount, quantity=1)
+    self.total += amount * quantity
+    quantity.times do
+      items << title
+    end
+    self.last_transaction = amount * quantity
   end
 
   def apply_discount
-    if @discount == 0
-      return "There is no discount to apply."
+    if discount != 0
+      self.total = (total * ((100.0 - discount.to_f)/100)).to_i
+      "After the discount, the total comes to $#{self.total}."
+    else
+      "There is no discount to apply."
     end
-    @total -= @total * @discount / 100
-    return "After the discount, the total comes to $#{@total}."
   end
 
-  def items
-    item_names = []
-    @cart.each do | item_info |
-      #test expects product name * quantity...
-      for qty in 1..item_info[:quantity]
-        item_names << item_info[:name]
-      end
-    end
-    item_names
-  end
   def void_last_transaction
-# subtracts the last transaction from the total
- self.total = self.total - self.last_transaction
-end
-
+    self.total = self.total - self.last_transaction
+  end
 end
